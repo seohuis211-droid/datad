76adffe16d9ec912094bc9918ae589e5b245741c
seohuis211-droid <seohuis211@gmail.com>
2026-07-05 03:32:51 +0900

    Update important.js

--- important.js
+++ important.js
@@ -1,4 +1,5 @@
-var urlList = [
+// important.js - 중요 링크 목록
+var importantList = [
     "https://tenpeuro.clickn.co.kr/",
     "https://tenpeuro.clickn.co.kr/pages/gangnamtenpro",
     "https://tenpeuro.clickn.co.kr/pages/gangnamten",
@@ -6,5 +7,5 @@ var urlList = [
     "https://tenpeuro.clickn.co.kr/pages/gangnamtenpeuro",
     "https://tenpeuro.clickn.co.kr/pages/nonhyeontenpro",
     "https://tenpeuro.clickn.co.kr/pages/nonhyeonten",
-    "https://tenpeuro.clickn.co.kr/pages/nonhyeon-tenpro",
+    "https://tenpeuro.clickn.co.kr/pages/nonhyeon-tenpro"
 ];
