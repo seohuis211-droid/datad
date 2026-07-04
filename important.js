db9cb738e5aadee58b508f06566b89ea8e7725ae
seohuis211-droid <seohuis211@gmail.com>
2026-07-05 05:03:43 +0900

    Update important.js

--- important.js
+++ important.js
@@ -1,11 +1,4 @@
 var importantLinks = [
     { url: "https://tenpeuro.clickn.co.kr/", title: "텐프로" },
-    { url: "https://tenpeuro.clickn.co.kr/pages/gangnamtenpro", title: "강남텐프로" },
-    { url: "https://tenpeuro.clickn.co.kr/pages/gangnamten", title: "강남텐" },
-    { url: "https://tenpeuro.clickn.co.kr/pages/gangnam-tenpro", title: "강남 텐프로" },
-    { url: "https://tenpeuro.clickn.co.kr/pages/gangnamtenpeuro", title: "강남텐프로" },
-    { url: "https://tenpeuro.clickn.co.kr/pages/nonhyeontenpro", title: "논현텐프로" },
-    { url: "https://tenpeuro.clickn.co.kr/pages/nonhyeonten", title: "논현텐" },
-    { url: "https://tenpeuro.clickn.co.kr/pages/nonhyeon-tenpro", title: "논현 텐프로" },
-    { url: "https://tenpeuro.clickn.co.kr/pages/nonhyeontenpeuro", title: "논현텐프로" }
+    { url: "https://tenpeuro.clickn.co.kr/pages/gangnamtenpro", title: "강남텐프로" }
 ];
