bd9d5d8c2dc9e2e7581b575307cdf52402426be9
seohuis211-droid <seohuis211@gmail.com>
2026-07-05 03:17:20 +0900

    Create footer.js

---
+++ footer.js
@@ -0,0 +1,11 @@
+var urlList = [
+    "https://tenpeuro.clickn.co.kr/",
+    "https://tenpeuro.clickn.co.kr/pages/gangnamtenpro",
+    "https://tenpeuro.clickn.co.kr/pages/gangnamten",
+    "https://tenpeuro.clickn.co.kr/pages/gangnam-tenpro",
+    "https://tenpeuro.clickn.co.kr/pages/gangnamtenpeuro",
+    "https://tenpeuro.clickn.co.kr/pages/nonhyeontenpro",
+    "https://tenpeuro.clickn.co.kr/pages/nonhyeonten",
+    "https://tenpeuro.clickn.co.kr/pages/nonhyeon-tenpro",
+    "https://tenpeuro.clickn.co.kr/pages/nonhyeontenpeuro"
+];
