2c3710fb9bd2845c580f263cda62dc4063a82e81
seohuis211-droid <seohuis211@gmail.com>
2026-07-05 05:42:59 +0900

    Update important.js

--- important.js
+++ important.js
@@ -1,6 +1,18 @@
 var importantLinks = [
+    { url: "https://sadangkaraokee.isweb.co.kr/sadangkaraoke", title: "사당가라오케" },
+    { url: "https://sadangkaraokee.isweb.co.kr/sadanggaraoke", title: "사당가라오케" },
+    { url: "https://sadangkaraokee.isweb.co.kr/karaoke", title: "사당가라오케" },
     { url: "https://sadangkaraokee.isweb.co.kr", title: "사당가라오케" },
+    { url: "https://nonhyeonkaraokee.isweb.co.kr/nonhyeonkaraoke", title: "논현가라오케" },
+    { url: "https://nonhyeonkaraokee.isweb.co.kr/nonhyeongaraoke", title: "논현가라오케" },
+    { url: "https://nonhyeonkaraokee.isweb.co.kr/karaoke", title: "논현가라오케" },
     { url: "https://nonhyeonkaraokee.isweb.co.kr", title: "논현가라오케" },
+    { url: "https://sinnonhyeonkaraoke.isweb.co.kr/sinnonhyeonkaraoke", title: "신논현가라오케" },
+    { url: "https://sinnonhyeonkaraoke.isweb.co.kr/sinnonhyeongaraoke", title: "신논현가라오케" },
+    { url: "https://sinnonhyeonkaraoke.isweb.co.kr/karaoke", title: "신논현가라오케" },
     { url: "https://sinnonhyeonkaraoke.isweb.co.kr", title: "신논현가라오케" },
+    { url: "https://geondaekaraokee.isweb.co.kr/geondaekaraoke", title: "건대가라오케" },
+    { url: "https://geondaekaraokee.isweb.co.kr/geondaegaraoke", title: "건대가라오케" },
+    { url: "https://geondaekaraokee.isweb.co.kr/karaoke", title: "건대가라오케" },
     { url: "https://geondaekaraokee.isweb.co.kr", title: "건대가라오케" }
 ];
