6ca5c2a804f8fe90e0909bf6dd433ba5f0c16396
seohuis211-droid <seohuis211@gmail.com>
2026-07-05 03:13:39 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,3 +1,13 @@
 var urlList = [
     "https://playzone.clickn.co.kr/pages/yeonsudong-publics",
+    "https://playzone.clickn.co.kr/pages/yongsan-yuheung",
+    "https://playzone.clickn.co.kr/pages/samsung-tencafes",
+    "https://playzone.clickn.co.kr/pages/isuyuheung",
+    "https://playzone.clickn.co.kr/pages/gimpo-karaoke",
+    "https://playzone.clickn.co.kr/pages/seohyunhyper",
+    "https://playzone.clickn.co.kr/pages/magok-tenpros",
+    "https://playzone.clickn.co.kr/pages/yeouido-karaoke",
+    "https://playzone.clickn.co.kr/pages/yeouido-public",
+    "https://playzone.clickn.co.kr/pages/michuholhyperpublics",
+    "https://playzone.clickn.co.kr/pages/yongsan-publics"
 ];
