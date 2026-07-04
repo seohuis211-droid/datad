3c26df248650fc5825437a4f2a12731eba57a9d2
seohuis211-droid <seohuis211@gmail.com>
2026-07-05 03:00:34 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,9 @@
+var urlList = [
+    "https://playzone.clickn.co.kr/pages/yeonsudong-publics",
+    "https://playzone.clickn.co.kr/pages/yongsan-yuheung",
+    "https://playzone.clickn.co.kr/pages/samsung-tencafes",
+    "https://playzone.clickn.co.kr/pages/isuyuheung",
+    "https://playzone.clickn.co.kr/pages/gimpo-karaoke",
+    "https://playzone.clickn.co.kr/pages/seohyunhyper",
+    "https://playzone.clickn.co.kr/pages/magok-tenpros"
+];
